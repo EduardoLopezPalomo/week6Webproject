0509a25955a8aa931cd7cbcf38ae5faa06b61b64
Eduardo <e.lpalomo@alumnos.upm.es>
2023-12-11 16:12:03 +0200

    as

--- index.ts
+++ index.ts
@@ -1,131 +1,41 @@
 import express, { Request, Response } from 'express';
-import {Greeting, IVehicle, ICar, IBoat, IPlane} from "./types";
-
-class GreetingMessage implements Greeting {
-  constructor(public message: string) {}
-}
-
-class Vehicle implements IVehicle{
-    constructor(
-        public model: string,
-        public color: string,
-        public year: number,
-        public power: number
-    ){}
-}
-class Car implements ICar{
-    constructor(
-        public model: string,
-        public color: string,
-        public year: number,
-        public power: number,
-        public bodyType: string,
-        public wheelCount: number
-    ){}
-}
-class Boat implements IBoat{
-    constructor(
-        public model: string,
-        public color: string,
-        public year: number,
-        public power: number,
-        public draft: number
-    ){}
-}
-class Plane implements IPlane{
-    constructor(
-        public model: string,
-        public color: string,
-        public year: number,
-        public power: number,
-        public wingspan: number
-    ){}
-}
+import { IVehicle, ICar, IBoat, IPlane} from "./types";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.get('/hello', (req: Request, res: Response) => {
+type Vehicle = IBoat | ICar | IPlane ; 
 
-  const greeting: Greeting = new GreetingMessage('Hello world');
-  res.send({message: 'Hello world'});
+let vehicleList: Vehicle[] = [];
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Test express Typescript");
 });
 
-let vehicleList: (Car | Boat | Plane | Vehicle)[] = [];
+app.get('/hello', (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+
 
 app.post('/vehicle/add', (req: Request, res: Response) => {
   const { model, color, year, power, bodyType, wheelCount, draft, wingspan } = req.body;
 
   if (model && color && year && power) {
-    if (bodyType !== undefined && wheelCount !== undefined) {
-
-      const newCar: Car = { model, color, year, power, bodyType, wheelCount };
-      vehicleList.push(newCar);
-      res.status(201).json({ message: 'Car added' });
-    } else if (draft !== undefined) {
-
-      const newBoat: Boat = { model, color, year, power, draft };
-      vehicleList.push(newBoat);
-      res.status(201).json({ message: 'Boat added' });
-    } else if (wingspan !== undefined) {
-
-      const newPlane: Plane = { model, color, year, power, wingspan };
-      vehicleList.push(newPlane);
-      res.status(201).json({ message: 'Plane added' });
-    } else {
-
-      const newVehicle : Vehicle = {model, color, year, power};
-      vehicleList.push(newVehicle);
-      res.status(201).json({message:'Vehicle added'});
-    }
+    const vehicle: Vehicle = req.body;
+    vehicleList.push(vehicle);
+    res.status(201).send("Vehicle added")
   } else {
     res.status(400).json({ message: 'Missing or invalid vehicle data' });
   }
 });
 
 app.get('/vehicle/search/:model', (req: Request, res: Response) => {
-    const { model } = req.params;
+    const  model  = req.params.model;
     const foundVehicle = vehicleList.find((vehicle) => vehicle.model === model);
 
     if (foundVehicle) {
-        if(foundVehicle instanceof Car){
-            let responseVehicle: Car = {
-                model: foundVehicle.model,
-                color: foundVehicle.color,
-                year: foundVehicle.year,
-                power: foundVehicle.power,
-                bodyType: foundVehicle.bodyType,
-                wheelCount: foundVehicle.wheelCount
-            };
-            res.status(200).json(responseVehicle);
-        } else if(foundVehicle instanceof Boat){
-            let responseVehicle: Boat = {
-                model: foundVehicle.model,
-                color: foundVehicle.color,
-                year: foundVehicle.year,
-                power: foundVehicle.power,
-                draft: foundVehicle.draft,
-            };
-            res.status(200).json(responseVehicle);
-        } else if(foundVehicle instanceof Plane){
-            let responseVehicle: Plane = {
-                model: foundVehicle.model,
-                color: foundVehicle.color,
-                year: foundVehicle.year,
-                power: foundVehicle.power,
-                wingspan: foundVehicle.wingspan,
-            };
-            res.status(200).json(responseVehicle);
-        } else if(foundVehicle instanceof Vehicle){
-            let responseVehicle: Vehicle = {
-                model: foundVehicle.model,
-                color: foundVehicle.color,
-                year: foundVehicle.year,
-                power: foundVehicle.power,
-            };
-            res.status(200).json(responseVehicle);
-        }
+        res.status(200).json(foundVehicle);
     } else {
       res.status(404).json({ message: 'Vehicle not found' });
     }
